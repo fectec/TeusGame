dc8f92d71b6ae04d103998e90695e0ce14b620ae
fectec <fectec151@gmail.com>
2023-09-15 11:09:20 -0600

    Added menu and hitboxes

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ pygame.init()
 # GLOBAL CONSTANTS
 
 SCREEN_WIDTH = 1200
-SCREEN_HEIGTH = 762
-SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGTH))
+SCREEN_HEIGHT = 762
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 RUNNING = [pygame.image.load(os.path.join("assets/protagonist", "protagonistRun1.png")),
            pygame.image.load(os.path.join("assets/protagonist", "protagonistRun2.png"))]
@@ -112,13 +112,20 @@ class Protagonist:
 class Enemy:
 
     def __init__ (self, image, type):
+        
         self.image = image
         self.type = type
+
         self.rect = self.image[self.type].get_rect()
         self.rect.x = SCREEN_WIDTH
+
+        self.collideRect = pygame.rect.Rect((0, 0), (self.rect.width * 0.60, self.rect.height * 0.60))
     
     def update (self):
+
         self.rect.x -= game_speed
+        self.collideRect.x -= game_speed
+
         if self.rect.x < -self.rect.width:
             obstacles.pop()
     
@@ -128,8 +135,13 @@ class Enemy:
 class Passerby(Enemy):
 
     def __init__(self, image):
+
         self.type = random.randint(0, 2)
         super().__init__(image, self.type)
+
+        #P
+        #V
+        #G
 
         if self.type == 0:
             self.rect.y = 623
@@ -138,39 +150,59 @@ class Passerby(Enemy):
         else:
             self.rect.y = 628
 
+        self.collideRect.center = self.rect.center
+
+        if self.type == 0:
+            self.collideRect.y = self.rect.y + 100
+            self.collideRect.x = SCREEN_WIDTH + 48
+        elif self.type == 1:
+            self.collideRect.y = self.rect.y + 100
+            self.collideRect.x = SCREEN_WIDTH + 60
+        else:
+            self.collideRect.y = self.rect.y + 100
+            self.collideRect.x = SCREEN_WIDTH + 27
+
 class Button:
     
     def __init__(self, x, y, image):
+
         self.image = image
         self.rect = self.image.get_rect()
-        self.rect.topleft = (x, y)
+        self.rect.center = (x, y)
         self.clicked = False
 
     def draw(self):
 
         pos = pygame.mouse.get_pos()
-        print(pos)
 
         if self.rect.collidepoint(pos):
             if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
                 self.clicked = True
-                print("a")
 
         if pygame.mouse.get_pressed()[0] == 0:
                 self.clicked = False
 
         SCREEN.blit(self.image, (self.rect.x, self.rect.y))
 
-""" def main_menu():
+def menu(death_count):
     
-    menuTitleFont = pygame.font.Font(os.path.join("assets/other", "arcadeFont.ttf"), 100)
+    MENU_TITLE_FONT = pygame.font.Font(os.path.join("assets/other", "arcadeFont.ttf"), 100)
+    MENU_SUBTITLE_FONT = pygame.font.Font(os.path.join("assets/other", "arcadeFont.ttf"), 40)
+   
+    TITLE_TEXT = MENU_TITLE_FONT.render("TEUS GAME", True, "#C1EB17")
+    TITLE_TEXT_RECT = TITLE_TEXT.get_rect()
+    TITLE_TEXT_RECT.center = (SCREEN_WIDTH // 2, SCREEN_WIDTH // 6)
+
+    START_BUTTON_IMAGE = pygame.image.load(os.path.join("assets/other", "startIcon.png"))
+    START_BUTTON = Button(SCREEN_WIDTH // 2, SCREEN_WIDTH // 6 * 3, START_BUTTON_IMAGE)
 
     pygame.mixer.init()
     pygame.mixer.music.load(os.path.join("assets/other", "menuMusic.mp3"))
     pygame.mixer.music.set_volume(0.7)
-    pygame.mixer.music.play()
+    #pygame.mixer.music.play()
 
     run = True
+    clock = pygame.time.Clock()
 
     while run:
 
@@ -178,34 +210,47 @@ class Button:
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        MENU_TEXT = menuTitleFont.render("TEUS GAME", True, "#DA8EE7")
-        TEXT_RECT = MENU_TEXT.get_rect()
-        TEXT_RECT.center = (600, 200)
-
-        START_BUTTON_IMAGE = pygame.image.load(os.path.join("assets/other", "startIcon.png"))
-
-        start_button = Button(0, 0, START_BUTTON_IMAGE)
+        if START_BUTTON.clicked == True:
+            SCREEN.fill("black")
+            main()
 
         SCREEN.blit(BG[1], (0, 0))
-        SCREEN.blit(MENU_TEXT, TEXT_RECT)
-        start_button.draw()
+        SCREEN.blit(TITLE_TEXT, TITLE_TEXT_RECT)
+        START_BUTTON.draw()
 
+        if death_count == 0:
+            SUBTITLE_TEXT = MENU_SUBTITLE_FONT.render("PRESS THE BUTTON TO START", True, "#83EBE7")
+        elif death_count > 0:
+            SUBTITLE_TEXT = MENU_SUBTITLE_FONT.render("PRESS THE BUTTON TO RESTART", True, "#83EBE7")
+
+            SCORE_TEXT = MENU_SUBTITLE_FONT.render("Your Score: " + str(points), True, "#9CD7EB")
+            SCORE_TEXT_RECT = SCORE_TEXT.get_rect()
+            SCORE_TEXT_RECT.center = (SCREEN_WIDTH // 2, SCREEN_WIDTH // 6 * 2 + (SCREEN_WIDTH // 24))
+
+            SCREEN.blit(SCORE_TEXT, SCORE_TEXT_RECT)
+        
+        SUBTITLE_TEXT_RECT = SUBTITLE_TEXT.get_rect()
+        SUBTITLE_TEXT_RECT.center = (SCREEN_WIDTH // 2, SCREEN_WIDTH // 6 * 2)
+
+        SCREEN.blit(SUBTITLE_TEXT, SUBTITLE_TEXT_RECT)
+        
+        clock.tick(30)
         pygame.display.update()
 
-    pygame.mixer.music.stop() """
+    pygame.mixer.music.stop()
 
 def main():
     
     global game_speed, x_pos_bg, y_pos_bg, points, obstacles
     
-    scoreFont = pygame.font.Font(os.path.join("assets/other", "arcadeFont.ttf"), 20)
+    SCORE_FONT = pygame.font.Font(os.path.join("assets/other", "arcadeFont.ttf"), 20)
     game_speed = 20
     x_pos_bg = 0
     y_pos_bg = 0
     points = 0
     obstacles = []
     death_count = 0
-
+    
     def score():
 
         global points, game_speed
@@ -215,10 +260,10 @@ def main():
         if points % 100 == 0:
             game_speed += 1
 
-        TEXT = scoreFont.render("Points: " + str(points), True, (0, 0, 0))
-        TEXT_RECT = TEXT.get_rect()
-        TEXT_RECT.center = (600, 35)
-        SCREEN.blit(TEXT, TEXT_RECT)
+        SCORE_TEXT = SCORE_FONT.render("Points: " + str(points), True, (0, 0, 0))
+        SCORE_TEXT_RECT = SCORE_TEXT.get_rect()
+        SCORE_TEXT_RECT.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 6)
+        SCREEN.blit(SCORE_TEXT, SCORE_TEXT_RECT)
 
     def background():
 
@@ -238,13 +283,11 @@ def main():
     pygame.display.set_caption('Teus Game')
     pygame.display.set_icon(pygame.image.load(os.path.join("assets/other", "gameIcon.png")))
 
-    #main_menu()
-
     pygame.mixer.music.load(os.path.join("assets/other", "slowMainMusic.mp3"))
     pygame.mixer.music.set_volume(0.7)
-    pygame.mixer.music.play()
+    #pygame.mixer.music.play()
 
-    player = Protagonist()
+    PLAYER = Protagonist()
 
     run = True
     clock = pygame.time.Clock()
@@ -261,8 +304,8 @@ def main():
 
         background()
 
-        player.draw()
-        player.update(userInput)
+        PLAYER.draw()
+        PLAYER.update(userInput)
 
         if len(obstacles) == 0:
             if random.randint(0, 2) == 0:
@@ -272,15 +315,14 @@ def main():
             obstacle.draw()
             obstacle.update()
 
-            if player.protagonist_rect.colliderect(obstacle.rect):
-                
-                print("a")
+            if PLAYER.protagonist_rect.colliderect(obstacle.collideRect):
+                pygame.time.delay(2000)
                 death_count += 1
-                #main_menu(death_count)
+                menu(death_count)
                     
         score()
 
         clock.tick(30)
         pygame.display.update()
 
-main()
+menu(death_count = 0)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,4 @@
-
-main()
+SUBTITLE_TEXT = menuSubtitleFont.render("Press any Key to Restart", True, (0, 0, 0))
+    MENU_SCORE_TEXT = menuSubtitleFont.render("Your Score: " + str(points), True, (0, 0, 0))
+    MENU_SCORE_TEXT_RECT = MENU_SCORE_TEXT.get_rect()
+    MENU_SCORE_TEXT_RECT.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 50)
